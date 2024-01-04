b7b4f2828e53e2aab229cdb9d506386f8c8137ac
MVPOmegaGD <127200399+MVPOmegaGD@users.noreply.github.com>
2024-01-05 01:29:21 +0700

    Complete uploading image function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,70 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase';
 
 const Profile = () => {
-  const {currentUser} = useSelector(state => state.user);
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  const { currentUser } = useSelector(state => state.user);
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+      }
+    );
+    (error) => {
+      setImageError(true);
+    }
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setFormData({ ...formData, profilePicture: downloadURL });
+      })
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Thông tin cá nhân</h1>
       <form className='flex flex-col gap-8'>
-        <img src={currentUser.profilePicture} alt='profile' className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2'/>
-        <input defaultValue={currentUser.username} type='text' id='username' placeholder='Username' className='bg-slate-100 rounded-lg p-3'/>
-        <input defaultValue={currentUser.email} type='email' id='email' placeholder='Email' className='bg-slate-100 rounded-lg p-3'/>
-        <input type='password' id='password' placeholder='Mật khẩu' className='bg-slate-100 rounded-lg p-3'/>
+        <input type='file' ref={fileRef} hidden accept='image/*' onChange={(e) => setImage(e.target.files[0])}/>
+        {/* firebase storage use:  
+      allow read;
+      allow write: if
+      request.resource.size < 5*1024*1024 &&
+      request.resource.contentType.matches('image/.*') */}
+        <img src={currentUser.profilePicture} alt='profile'
+          className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2'
+          onClick={() => fileRef.current.click()} />
+        <p className='text-sm self-center'>
+          {
+            imageError ? (
+              <span className='text-red-700'>Lỗi tải ảnh (File ảnh nhỏ hơn 5MB)</span>
+            ) : imagePercent > 0 && imagePercent < 100 ? (
+              <span className='text-slate-700'>{`Đang tải được ${imagePercent}%`}</span>
+            ) : imagePercent === 100 ? (
+              <span className='text-green-700'>Ảnh được tải thành công</span>
+            ) : ''
+          }
+        </p>
+        <input defaultValue={currentUser.username} type='text' id='username' placeholder='Username' className='bg-slate-100 rounded-lg p-3' />
+        <input defaultValue={currentUser.email} type='email' id='email' placeholder='Email' className='bg-slate-100 rounded-lg p-3' />
+        <input type='password' id='password' placeholder='Mật khẩu' className='bg-slate-100 rounded-lg p-3' />
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Cập nhật</button>
       </form>
       <div className='flex justify-between mt-5'>
